26df65d220b20f252fec7ecf4a1074b38f6ebfc9
danid <danidiezsanchez@gmail.com>
2024-08-24 17:15:35 +0200

    first commit

---
+++ src/main/java/com/inditex/price_api_demo/PriceApiDemoApplication.java
@@ -0,0 +1,13 @@
+package com.inditex.price_api_demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PriceApiDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PriceApiDemoApplication.class, args);
+	}
+
+}
